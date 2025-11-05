ab2104b0c50fda7894fc1468042c6020160803d8
Yodmayom <Yodmayom3@gmail.com>
2025-11-05 20:47:01 +0700

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/react-myL2',
   plugins: [
     react({
       babel: {
